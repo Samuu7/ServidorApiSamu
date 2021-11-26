3c820cc0d1ea99b16a8c6b330e05bb9b7a34c2f8
Samu Osuna <sosuna.dam@institutcampalans.net>
2021-11-26 20:49:45 +0100

    Versio 1

---
+++ Projecte/Projecte/Pestanya1.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Projecte.Entity;
+using Projecte.Service;
+
+namespace Projecte
+{
+    /// <summary>
+    /// Lógica de interacción para Pestanya1.xaml
+    /// </summary>
+    public partial class Pestanya1 : Window
+    {
+
+        
+        public Pestanya1()
+        {
+            InitializeComponent();
+
+           listbox_1.ItemsSource = ResponsableService.GetAll();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+            Responsable oresp = new Responsable();
+            oresp.Name = nom_entrat.Text;
+            ResponsableService.Add(oresp);
+            listbox_1.ItemsSource = ResponsableService.GetAll();
+
+            nom_entrat.Clear();
+
+        }
+        public void TextBox_responsable_1(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = (TextBox)sender;
+            tb.Text = string.Empty;
+            tb.GotFocus -= TextBox_responsable_1;
+        }
+        
+        private void butto_eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            int index = listbox_1.SelectedIndex;
+            Responsable tb = (Responsable)listbox_1.Items[index];
+            ResponsableService.Delete(tb.ID);
+
+            listbox_1.ItemsSource = ResponsableService.GetAll();
+
+            //listbox_1.Items.RemoveAt(listbox_1.SelectedIndex);
+            /*Responsable oresp = new Responsable();
+            oresp.Name = nom_entrat.Text;
+            ResponsableService resp = new ResponsableService();
+            resp.(oresp);*/
+
+
+        }
+    }
+}
